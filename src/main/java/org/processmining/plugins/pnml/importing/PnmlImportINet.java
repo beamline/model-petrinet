08013e65188571b0da9fe8c44b59420e03cae34c
Andrea Burattin <andrea.burattin@gmail.com>
2022-03-04 10:32:52 +0100

    Added Petri net model

---
+++ src/main/java/org/processmining/plugins/pnml/importing/PnmlImportINet.java
@@ -0,0 +1,41 @@
+package org.processmining.plugins.pnml.importing;
+
+import java.io.InputStream;
+
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import org.processmining.framework.abstractplugins.AbstractImportPlugin;
+import org.processmining.framework.plugin.PluginContext;
+import org.processmining.framework.plugin.annotations.Plugin;
+import org.processmining.models.graphbased.directed.petrinet.InhibitorNet;
+import org.processmining.models.graphbased.directed.petrinet.PetrinetGraph;
+import org.processmining.models.graphbased.directed.petrinet.impl.PetrinetFactory;
+import org.processmining.models.semantics.petrinet.Marking;
+import org.processmining.plugins.pnml.base.Pnml;
+
+public class PnmlImportINet extends AbstractImportPlugin {
+
+	protected FileFilter getFileFilter() {
+		return new FileNameExtensionFilter("PNML files", "pnml");
+	}
+
+	protected Object importFromStream(PluginContext context, InputStream input, String filename, long fileSizeInBytes)
+			throws Exception {
+		PnmlImportUtils utils = new PnmlImportUtils();
+		Pnml pnml = utils.importPnmlFromStream(context, input, filename, fileSizeInBytes);
+		if (pnml == null) {
+			/*
+			 * No PNML found in file. Fail.
+			 */
+			return null;
+		}
+		/*
+		 * PNML file has been imported. Now we need to convert the contents to
+		 * an Inhibitor net.
+		 */
+		PetrinetGraph net = PetrinetFactory.newInhibitorNet(pnml.getLabel() + " (imported from " + filename + ")");
+
+		return utils.connectNet(context, pnml, net);
+	}
+}
